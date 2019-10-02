82f02df7a49301ccd1cd9e1d16e82d1053b27837
Tyler Keith <55261569+keitht22@users.noreply.github.com>
2019-10-01 21:50:11 -0400

    Update mtxToPy.py

--- mtxToPy.py
+++ mtxToPy.py
@@ -1,7 +1,7 @@
 from scipy.io import mmread
 from scipy.sparse import csr_matrix
 
-f = mmread('/Users/tylerkeith/Documents/Research/BatchLayoutCode-master/datasets/input/3elt_dual.mtx')
+f = mmread('./3elt_dual.mtx')
 f = f.toarray()
 
 print(f)
