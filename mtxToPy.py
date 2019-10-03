7c2e8db10616e38760a777c820c7dd1ab93fe2ec
Mohammad Khaledur Rahman <morahma@v-login1.victor.futuresystems.org>
2019-10-03 14:05:29 -0400

    edited

--- mtxToPy.py
+++ mtxToPy.py
@@ -1,11 +1,17 @@
 from scipy.io import mmread
 from scipy.sparse import csr_matrix
+import sys
+import networkx as nx
 
-f = mmread('./3elt_dual.mtx')
-f = f.toarray()
+def readInput(filename):
+	f = mmread(filename)
+	f = f.toarray()
+	print(f)
+	c = csr_matrix(f)
+	return c
 
-print(f)
-
-c = csr_matrix(f)
-
-print(c)
+if __name__ == "__main__":
+	#"./3elt_dual.mtx"
+	filen = sys.argv[1]
+	#print(sys.argv[0], sys.argv[1])
+	mat = readInput(filen)
